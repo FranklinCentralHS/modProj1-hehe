1fcd5aa86fafffe7bdfd453151edee6510be5491
EGRim-2009 <egoar@ftstudent.org>
2024-08-20 13:49:22 -0400

    rectangles

--- Node2D.gd
+++ Node2D.gd
@@ -3,11 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	label(0);
 	pass # Replace with function body.
 func _draw():
-	draw_rect(Rect2(256.0,8.0,128.0,50.0), Color.DARK_BLUE);
-
+	draw_rect(Rect2(256.0,0.0,128.0,50.0), Color.DARK_BLUE);
+	draw_rect(Rect2(250.0,340.0,128.0,400.0), Color.DARK_BLUE);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 	
